c221b3ee06075dbca8660793908443e98fdc55dd
ia7ck <23146842+ia7ck@users.noreply.github.com>
2020-09-02 20:28:18 +0900

    fix chmin/chmax

--- src/util.rs
+++ src/util.rs
@@ -1,14 +1,14 @@
 #[allow(unused_macros)]
 macro_rules! chmin {
     ($a:expr, $b:expr) => {
-        std::cmp::min($a, $b)
+        $a = std::cmp::min($a, $b)
     };
 }
 
 #[allow(unused_macros)]
 macro_rules! chmax {
     ($a:expr, $b:expr) => {
-        std::cmp::max($a, $b)
+        $a = std::cmp::max($a, $b)
     };
 }
 
@@ -16,13 +16,21 @@ macro_rules! chmax {
 mod tests {
     #[test]
     fn chmin_test() {
-        assert_eq!(chmin!(123, 4), 4);
-        assert_eq!(chmin!(1, 234), 1);
+        let mut a = 123;
+        chmin!(a, 4);
+        assert_eq!(a, 4);
+        let mut b = 1;
+        chmin!(b, 234);
+        assert_eq!(b, 1);
     }
 
     #[test]
     fn chmax_test() {
-        assert_eq!(chmax!(123, 4), 123);
-        assert_eq!(chmax!(1, 234), 234);
+        let mut a = 123;
+        chmax!(a, 4);
+        assert_eq!(a, 123);
+        let mut b = 1;
+        chmax!(b, 234);
+        assert_eq!(b, 234);
     }
 }
