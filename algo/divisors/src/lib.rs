a412d84133099ab5fa4a582890aa2ceda5807310
ia7ck <23146842+ia7ck@users.noreply.github.com>
2021-02-07 20:32:22 +0900

    add comment

--- algo/divisors/src/lib.rs
+++ algo/divisors/src/lib.rs
@@ -1,4 +1,15 @@
-pub fn divisors(n: i64) -> Vec<i64> {
+/// 非負整数 `n` の全ての約数を返します。
+///
+/// - 約数は昇順に並んでいることが保証されます。
+/// - `0` を渡すと空のベクタ `vec![]` を返します。
+///
+/// # Examples
+/// ```
+/// use divisors::divisors;
+///
+/// assert_eq!(divisors(24), vec![1, 2, 3, 4, 6, 8, 12, 24]);
+/// ```
+pub fn divisors(n: u64) -> Vec<u64> {
     let mut res = vec![];
     let mut large = vec![];
     for k in (1..).take_while(|k| k * k <= n) {
@@ -20,6 +31,7 @@ mod tests {
 
     #[test]
     fn divisors_test() {
+        assert_eq!(divisors(0), vec![]);
         assert_eq!(divisors(1), vec![1]);
         assert_eq!(divisors(2), vec![1, 2]);
         assert_eq!(divisors(24), vec![1, 2, 3, 4, 6, 8, 12, 24]);
