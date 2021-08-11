1677e87d557db8a21e9d500af7b851668775ea6c
ia7ck <23146842+ia7ck@users.noreply.github.com>
2021-08-12 01:17:46 +0900

    add lcp_array test

---
+++ algo/suffix_array/examples/number_of_substrings.rs
@@ -0,0 +1,16 @@
+// oj: https://judge.yosupo.jp/problem/number_of_substrings
+use proconio::marker::Chars;
+use proconio::{fastout, input};
+use suffix_array::{lcp_array, suffix_array};
+
+#[fastout]
+fn main() {
+    input! {
+        s: Chars,
+    }
+
+    let sa = suffix_array(&s);
+    let lcp = lcp_array(&s, &sa);
+    let ans = s.len() * (s.len() + 1) / 2 - lcp.iter().sum::<usize>();
+    println!("{}", ans);
+}
