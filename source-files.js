9f77fa6d55045b8ae4415e84d50a1b8b5d6ef003
ia7ck <ia7ck@users.noreply.github.com>
2021-08-14 10:59:40 +0000

    Deploying to gh-pages from  @ 9b99470b7565be7d33b6c5e66866a8a5846033de 🚀

--- source-files.js
+++ source-files.js
@@ -1,6 +1,7 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["binary_search"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["ceil_log2"] = {"name":"","files":["lib.rs"]};
+sourcesIndex["detect_cycle"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["dijkstra"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["divisors"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["factorials"] = {"name":"","files":["lib.rs"]};
