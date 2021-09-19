22e993fb60d6907ebcca3c27631121c24451d45c
ia7ck <23146842+ia7ck@users.noreply.github.com>
2021-09-19 19:49:20 +0900

    support special judge

---
+++ others/special_judge_example/examples/aplusb_special.rs
@@ -0,0 +1,13 @@
+// oj: https://judge.yosupo.jp/problem/aplusb
+// oj_judge_rs_program: ./judge.rs
+use proconio::input;
+use special_judge_example::add;
+
+fn main() {
+    input! {
+        a: u32,
+        b: u32,
+    }
+    let ans = add(a, b);
+    println!("{}", ans);
+}
---
+++ others/special_judge_example/examples/judge.rs
@@ -0,0 +1,59 @@
+use proconio::{
+    input,
+    source::{once::OnceSource, Source},
+};
+
+use std::env;
+use std::fs::File;
+use std::io;
+use std::io::{BufRead, BufReader};
+
+struct Input {
+    a: u32,
+    b: u32,
+}
+
+struct Output {
+    sum: u32,
+}
+
+fn read_input(input: impl BufRead) -> Input {
+    let mut source = OnceSource::new(input);
+    input! {
+        from &mut source,
+        a: u32,
+        b: u32,
+    }
+    assert!(source.is_empty());
+    Input { a, b }
+}
+
+fn read_output(output: impl BufRead) -> Output {
+    let mut source = OnceSource::new(output);
+    input! {
+        from &mut source,
+        sum: u32,
+    }
+    assert!(source.is_empty());
+    Output { sum }
+}
+
+fn main() -> io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    assert_eq!(args.len(), 4);
+    let (input, my_output, expected_output) = (&args[1], &args[2], &args[3]);
+
+    let input = File::open(input)?;
+    let input = read_input(BufReader::new(input));
+
+    let my_output = File::open(my_output)?;
+    let my_output = read_output(BufReader::new(my_output));
+
+    let expected_output = File::open(expected_output)?;
+    let expected_output = read_output(BufReader::new(expected_output));
+
+    assert_eq!(my_output.sum, expected_output.sum);
+    assert_eq!(my_output.sum, input.a + input.b);
+
+    Ok(())
+}
