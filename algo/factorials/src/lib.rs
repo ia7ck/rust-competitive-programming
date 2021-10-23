bb71c88a2740f0755599904530073ede131bc581
ia7ck <23146842+ia7ck@users.noreply.github.com>
2021-10-23 19:18:31 +0900

    rename

--- algo/factorials/src/lib.rs
+++ algo/factorials/src/lib.rs
@@ -1,9 +1,9 @@
-/// `1` 以上 `size` 未満の `n` について、`n` の階乗 (mod `mo`) と、その乗法逆元を O(`size`) で計算します。
+/// `1` 以上 `size` 未満の `n` について、`n` の階乗 (mod `modulo`) と、その乗法逆元を O(`size`) で計算します。
 ///
 /// 逆元を正しく計算するためには
 ///
-/// - `mo` が素数
-/// - `mo >= size`
+/// - `modulo` が素数
+/// - `modulo >= size`
 ///
 /// である必要があります。
 ///
@@ -16,7 +16,7 @@
 ///     assert_eq!(fac[i] * fac_inv[i] % p, 1);
 /// }
 /// ```
-pub fn factorials(size: usize, mo: u64) -> (Vec<u64>, Vec<u64>) {
+pub fn factorials(size: usize, modulo: u64) -> (Vec<u64>, Vec<u64>) {
     let mut fac = vec![0; size];
     let mut inv = vec![0; size];
     let mut inv_fac = vec![0; size];
@@ -27,9 +27,9 @@ pub fn factorials(size: usize, mo: u64) -> (Vec<u64>, Vec<u64>) {
     inv_fac[1] = 1;
     for i in 2..size {
         let i_u64 = i as u64;
-        fac[i] = fac[i - 1] * i_u64 % mo;
-        inv[i] = ((mo - inv[(mo as usize) % i]) * (mo / i_u64)).rem_euclid(mo);
-        inv_fac[i] = inv_fac[i - 1] * inv[i] % mo;
+        fac[i] = fac[i - 1] * i_u64 % modulo;
+        inv[i] = ((modulo - inv[(modulo as usize) % i]) * (modulo / i_u64)).rem_euclid(modulo);
+        inv_fac[i] = inv_fac[i - 1] * inv[i] % modulo;
     }
     (fac, inv_fac)
 }
