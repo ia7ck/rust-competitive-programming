966ba5a84194379de1262cb482e332e9496d314f
ia7ck <23146842+ia7ck@users.noreply.github.com>
2021-11-06 21:15:02 +0900

    move to lib

--- oj_test/src/main.rs
+++ oj_test/src/main.rs
@@ -1,118 +1,10 @@
-use anyhow::{ensure, Result};
+use std::env;
+
+use anyhow::Result;
 use glob::glob;
 use log::info;
 
-use std::collections::HashMap;
-use std::env;
-use std::fs;
-use std::path::{Path, PathBuf};
-use std::process::Command;
-
-struct ProblemSolver {
-    solver_path: PathBuf,
-    test_property: TestProperty,
-}
-
-impl ProblemSolver {
-    fn new(path: &Path) -> Self {
-        Self {
-            solver_path: path.to_path_buf(),
-            test_property: TestProperty::from(path),
-        }
-    }
-
-    fn run<T>(&self, testcase: T) -> Result<()>
-    where
-        T: Testcase,
-    {
-        testcase.setup()?;
-
-        let solver_name = self.solver_path.file_stem().unwrap().to_string_lossy();
-        let solve_command = format!("cargo run --quiet --release --example {}", solver_name);
-
-        let mut oj_command = Command::new("oj");
-        oj_command
-            .arg("test")
-            .arg("--directory")
-            .arg(testcase.testcase_dir().as_os_str())
-            .arg("--command")
-            .arg(solve_command);
-
-        if std::env::consts::OS != "windows" {
-            oj_command.arg("--jobs").arg("2");
-        }
-
-        // special judge
-        // for problem which has multiple answers
-        if let Some(judge_program_path) = self.judge_program_path() {
-            let judge_name = judge_program_path.file_stem().unwrap().to_string_lossy();
-            let judge_command = format!("cargo run --quiet --release --example {}", judge_name);
-            oj_command.arg("--judge-command").arg(judge_command);
-        }
-
-        info!("execute {:?}", oj_command);
-        let status = oj_command.status()?;
-        ensure!(status.success(), "failed: oj test");
-
-        Ok(())
-    }
-
-    fn problem_url(&self) -> Option<&str> {
-        self.test_property.get("problem")
-    }
-
-    fn judge_program_path(&self) -> Option<PathBuf> {
-        self.test_property
-            .get("judge_program_rs")
-            .map(|judge_program_rs| self.solver_path.parent().unwrap().join(judge_program_rs))
-    }
-}
-
-trait Testcase {
-    fn setup(&self) -> Result<()>;
-    fn testcase_dir(&self) -> PathBuf;
-}
-
-struct OnlineJudgeTestcase {
-    dir: PathBuf,
-    problem_url: String,
-}
-
-impl OnlineJudgeTestcase {
-    fn new(dir: &Path, problem_url: &str) -> Self {
-        Self {
-            dir: dir.to_path_buf(),
-            problem_url: problem_url.to_string(),
-        }
-    }
-}
-
-impl Testcase for OnlineJudgeTestcase {
-    // download testcase
-    fn setup(&self) -> Result<()> {
-        let testcase_dir = self.testcase_dir();
-        if testcase_dir.exists() {
-            // clear temporary directory
-            fs::remove_dir_all(testcase_dir.as_path()).unwrap_or_else(|err| panic!(err));
-        }
-        let mut oj_command = Command::new("oj");
-        oj_command
-            .arg("download")
-            .arg(self.problem_url.as_str())
-            .arg("--directory")
-            .arg(testcase_dir.as_os_str())
-            .arg("--system")
-            .arg("--silent");
-        info!("execute {:?}", oj_command);
-        let status = oj_command.status()?;
-        ensure!(status.success(), "failed: oj download");
-        Ok(())
-    }
-
-    fn testcase_dir(&self) -> PathBuf {
-        self.dir.clone()
-    }
-}
+use oj_test::{OnlineJudgeTestcase, ProblemSolver};
 
 fn main() -> Result<()> {
     env_logger::init();
@@ -122,72 +14,16 @@ fn main() -> Result<()> {
         let path = entry?;
         solvers.push(ProblemSolver::new(path.as_path()));
     }
-    solvers.sort_by(|s1, s2| s1.solver_path.cmp(&s2.solver_path));
+    solvers.sort_by(|s1, s2| s1.solver_path().cmp(&s2.solver_path()));
     for s in solvers {
         if let Some(problem_url) = s.problem_url() {
-            let testcase_dir = env::temp_dir().join(s.solver_path.with_extension(""));
+            let testcase_dir = env::temp_dir().join(s.solver_path().with_extension(""));
             let testcase = OnlineJudgeTestcase::new(testcase_dir.as_path(), problem_url);
             s.run(testcase)?;
         } else {
-            info!("skip {:?}", s.solver_path);
+            info!("skip {:?}", s);
         }
     }
 
     Ok(())
-}
-
-struct TestProperty {
-    properties: HashMap<String, String>,
-}
-
-impl TestProperty {
-    fn new(solver_source_code: &str) -> Self {
-        let mut properties = HashMap::new();
-        for l in solver_source_code.lines() {
-            let v: Vec<&str> = l.splitn(2, ':').map(|t| t.trim()).collect();
-            if v.len() != 2 {
-                continue;
-            }
-            if !v[0].starts_with("//") {
-                continue;
-            }
-            let key = v[0].trim_start_matches('/').trim();
-            properties.insert(key.to_string(), v[1].to_string());
-        }
-        Self { properties }
-    }
-
-    fn from(solver_path: &Path) -> Self {
-        let source_code = fs::read_to_string(solver_path).unwrap_or_else(|err| panic!(err));
-        Self::new(&source_code)
-    }
-
-    fn get(&self, key: &str) -> Option<&str> {
-        self.properties.get(key).map(|s| s.as_str())
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::TestProperty;
-
-    #[test]
-    fn parse_property_test() {
-        let source_code = r#"// problem1: https://example1.com
-//problem2: https://example2.com
- // problem3:https://example3.com
-// problem4 : https://example4.com
-
-// judge_program_rs: ./my_judge.rs
-fn main() {
-// return;
-}"#;
-        let property = TestProperty::new(source_code);
-        assert_eq!(property.get("problem1"), Some("https://example1.com"));
-        assert_eq!(property.get("problem2"), Some("https://example2.com"));
-        assert_eq!(property.get("problem3"), Some("https://example3.com"));
-        assert_eq!(property.get("problem4"), Some("https://example4.com"));
-        assert_eq!(property.get("judge_program_rs"), Some("./my_judge.rs"));
-        assert_eq!(property.get("return"), None);
-    }
 }
