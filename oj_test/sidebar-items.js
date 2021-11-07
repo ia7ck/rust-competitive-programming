163fed2f16e86901795ec4459925db6877db4445
ia7ck <23146842+ia7ck@users.noreply.github.com>
2021-11-07 03:29:49 +0000

    Deploying to gh-pages from  @ 81d9314354dfb6f1cf09f0e645a6912278182710 🚀

--- oj_test/sidebar-items.js
+++ oj_test/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["check_oj_version",""]],"struct":[["OnlineJudgeTestcase",""],["ProblemSolver",""]],"trait":[["Testcase",""]]});
+initSidebarItems({"fn":[["check_oj_version",""],["exists_artifacts",""]],"struct":[["OnlineJudgeTestcase",""],["ProblemSolver",""]],"trait":[["Testcase",""]]});
