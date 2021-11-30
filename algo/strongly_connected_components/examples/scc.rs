17ff4dde5a465a71e95a77f1ccf516bf3447bf3c
ia7ck <23146842+ia7ck@users.noreply.github.com>
2021-12-01 00:28:38 +0900

    add scc

---
+++ algo/strongly_connected_components/examples/scc.rs
@@ -0,0 +1,22 @@
+// problem: https://judge.yosupo.jp/problem/scc
+// judge_program_rs: ./judge_scc.rs
+
+use join::Join;
+use proconio::input;
+
+use strongly_connected_components::strongly_connected_components;
+
+fn main() {
+    input! {
+        n: usize,
+        m: usize,
+        edges: [(usize, usize); m],
+    };
+
+    let scc = strongly_connected_components(n, edges.iter().copied());
+    println!("{}", scc.len());
+    for com in scc {
+        print!("{} ", com.len());
+        println!("{}", com.iter().join(" "));
+    }
+}
