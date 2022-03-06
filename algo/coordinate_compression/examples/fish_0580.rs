2ffaf09a49ab9f3736813774a405e39010aec1a7
ia7ck <23146842+ia7ck@users.noreply.github.com>
2022-03-06 18:56:20 +0900

    rename, change interface

--- algo/coordinate_compression/examples/fish_0580.rs
+++ algo/coordinate_compression/examples/fish_0580.rs
@@ -1,6 +1,6 @@
 // problem: https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=0580
 
-use coordinate_compression::CoordinateCompression;
+use coordinate_compression::OrderMap;
 use proconio::input;
 
 fn main() {
@@ -10,19 +10,19 @@ fn main() {
         xydxyd: [(u64, u64, u64, u64, u64, u64); n],
     }
 
-    let cx: CoordinateCompression<u64> = xydxyd
+    let cx: OrderMap<u64> = xydxyd
         .iter()
         .copied()
         .map(|(x1, _, _, x2, _, _)| vec![x1, x2])
         .flatten()
         .collect();
-    let cy: CoordinateCompression<u64> = xydxyd
+    let cy: OrderMap<u64> = xydxyd
         .iter()
         .copied()
         .map(|(_, y1, _, _, y2, _)| vec![y1, y2])
         .flatten()
         .collect();
-    let cz: CoordinateCompression<u64> = xydxyd
+    let cz: OrderMap<u64> = xydxyd
         .iter()
         .copied()
         .map(|(_, _, z1, _, _, z2)| vec![z1, z2])
@@ -31,9 +31,9 @@ fn main() {
 
     let mut freq = vec![vec![vec![0; n * 2]; n * 2]; n * 2];
     for (x1, y1, z1, x2, y2, z2) in &xydxyd {
-        for x in cx.find_index(x1)..cx.find_index(x2) {
-            for y in cy.find_index(y1)..cy.find_index(y2) {
-                for z in cz.find_index(z1)..cz.find_index(z2) {
+        for x in cx.ord(x1)..cx.ord(x2) {
+            for y in cy.ord(y1)..cy.ord(y2) {
+                for z in cz.ord(z1)..cz.ord(z2) {
                     freq[x][y][z] += 1;
                 }
             }
@@ -44,7 +44,9 @@ fn main() {
         for y in 0..(n * 2 - 1) {
             for z in 0..(n * 2 - 1) {
                 if freq[x][y][z] >= k {
-                    ans += (cx[x + 1] - cx[x]) * (cy[y + 1] - cy[y]) * (cz[z + 1] - cz[z]);
+                    ans += (cx.at(x + 1) - cx.at(x))
+                        * (cy.at(y + 1) - cy.at(y))
+                        * (cz.at(z + 1) - cz.at(z));
                 }
             }
         }
