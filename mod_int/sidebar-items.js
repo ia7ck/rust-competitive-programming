b1358f086ffd9620376d82a7b0b5b56c93027c8e
ia7ck <23146842+ia7ck@users.noreply.github.com>
2022-04-16 15:06:54 +0000

    Deploying to gh-pages from  @ 3cf35dd919a6604b1185c244da5be7f3adf99349 🚀

--- mod_int/sidebar-items.js
+++ mod_int/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"macro":[["define_mod_int_p","好きな法の `ModInt` を定義します。"]],"struct":[["Mod1000000007",""],["Mod998244353",""],["ModInt",""]],"trait":[["Modulo",""]],"type":[["ModInt1000000007",""],["ModInt998244353",""]]});
+initSidebarItems({"macro":[["define_modulo","好きな法の `Modulo` を定義します。"]],"struct":[["Mod1000000007",""],["Mod998244353",""],["ModInt",""]],"trait":[["Modulo",""]],"type":[["ModInt1000000007",""],["ModInt998244353",""]]});
