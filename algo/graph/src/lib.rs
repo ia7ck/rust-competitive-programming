3f18dbe3032d917bfe9cfc8c6b0753bbbfa56710
ia7ck <23146842+ia7ck@users.noreply.github.com>
2022-12-04 09:39:38 +0000

    check tree graph

---
+++ algo/graph/src/lib.rs
@@ -0,0 +1,51 @@
+pub fn is_tree(n: usize, edges: &[(usize, usize)]) -> bool {
+    fn dfs(i: usize, g: &[Vec<usize>], visited: &mut Vec<bool>) {
+        for &j in &g[i] {
+            if visited[j] {
+                continue;
+            }
+            visited[j] = true;
+            dfs(j, g, visited);
+        }
+    }
+
+    for &(a, b) in edges {
+        assert!(a < n);
+        assert!(b < n);
+    }
+
+    if n == 0 {
+        return true;
+    }
+
+    if edges.len() != n - 1 {
+        return false;
+    }
+
+    let mut g = vec![vec![]; n];
+    for &(a, b) in edges {
+        g[a].push(b);
+        g[b].push(a);
+    }
+    let mut visited = vec![false; n];
+    visited[0] = true;
+    dfs(0, &g, &mut visited);
+    visited.iter().filter(|&&f| f).count() == n
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::is_tree;
+
+    #[test]
+    fn test_small() {
+        assert_eq!(is_tree(0, &[]), true);
+        assert_eq!(is_tree(1, &[]), true);
+        assert_eq!(is_tree(2, &[(0, 1)]), true);
+        assert_eq!(is_tree(3, &[(0, 1), (1, 2)]), true);
+        assert_eq!(is_tree(4, &[(0, 1), (0, 2), (0, 3)]), true);
+        assert_eq!(is_tree(4, &[(0, 1), (1, 2), (0, 3)]), true);
+        assert_eq!(is_tree(4, &[(0, 1), (2, 3)]), false);
+        assert_eq!(is_tree(4, &[(0, 1), (1, 2), (2, 0)]), false);
+    }
+}
