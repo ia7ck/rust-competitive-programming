3c1c5245d415b326e43db230c680f43c60cc1a93
ia7ck <23146842+ia7ck@users.noreply.github.com>
2023-07-25 15:23:36 +0000

    empty slice

--- algo/binary_search_range/src/lib.rs
+++ algo/binary_search_range/src/lib.rs
@@ -20,9 +20,7 @@ impl<T: Ord> BinarySearchRange<T> for [T] {
     /// assert_eq!(a.range(4..8), 2..6); // [      4, 5, 7, 7]
     /// ```
     fn range(&self, range: ops::Range<T>) -> ops::Range<usize> {
-        if self.is_empty() {
-            return 0..0;
-        }
+        assert!(!self.is_empty());
 
         let first_ge = |x: &T| {
             if x.le(&self[0]) {
@@ -58,8 +56,13 @@ mod tests {
     use crate::BinarySearchRange;
 
     #[test]
+    #[should_panic]
+    fn test_panic() {
+        vec![].range(0..10);
+    }
+
+    #[test]
     fn test_empty() {
-        assert_eq!(vec![].range(0..10).len(), 0);
         assert_eq!(vec![3, 4, 5].range(0..3).len(), 0);
         assert_eq!(vec![3, 4, 5].range(4..4).len(), 0);
         assert_eq!(vec![3, 4, 5].range(6..8).len(), 0);
