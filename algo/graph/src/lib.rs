7ebab0e29bcc1962f9262a001bf58323b487c495
ia7ck <23146842+ia7ck@users.noreply.github.com>
2024-09-30 00:00:10 +0900

    木の隣接リストから親頂点への辺を除く

--- algo/graph/src/lib.rs
+++ algo/graph/src/lib.rs
@@ -1,3 +1,5 @@
+use std::mem;
+
 pub fn is_tree(n: usize, edges: &[(usize, usize)]) -> bool {
     for &(a, b) in edges {
         assert!(a < n);
@@ -34,9 +36,44 @@ pub fn connectivity(n: usize, edges: &[(usize, usize)]) -> bool {
     visited.iter().filter(|&&f| f).count() == n
 }
 
+pub fn tree_drop_parent(
+    n: usize,
+    root: usize,
+    edges: &[(usize, usize)],
+) -> (Vec<Vec<usize>>, Vec<usize>) {
+    debug_assert!(is_tree(n, edges));
+
+    fn dfs(i: usize, p: usize, g: &Vec<Vec<usize>>, parent: &mut Vec<usize>) {
+        parent[i] = p;
+        for &j in &g[i] {
+            if j == p {
+                continue;
+            }
+            dfs(j, i, g, parent);
+        }
+    }
+
+    let mut g = vec![vec![]; n];
+    for &(a, b) in edges {
+        g[a].push(b);
+        g[b].push(a);
+    }
+    let mut parent = vec![usize::MAX; n];
+    dfs(root, root, &g, &mut parent);
+
+    for i in 0..n {
+        g[i] = mem::take(&mut g[i])
+            .into_iter()
+            .filter(|&j| j != parent[i])
+            .collect();
+    }
+
+    (g, parent)
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::is_tree;
+    use crate::{is_tree, tree_drop_parent};
 
     #[test]
     fn test_is_tree_small() {
@@ -48,5 +85,17 @@ mod tests {
         assert_eq!(is_tree(4, &[(0, 1), (1, 2), (0, 3)]), true);
         assert_eq!(is_tree(4, &[(0, 1), (2, 3)]), false);
         assert_eq!(is_tree(4, &[(0, 1), (1, 2), (2, 0)]), false);
+    }
+
+    #[test]
+    fn test_drop_parent() {
+        assert_eq!(
+            // 0 (root) -- 1 -- 2 -- 3
+            tree_drop_parent(4, 0, &[(0, 1), (1, 2), (2, 3)]),
+            (
+                vec![vec![1], vec![2], vec![3], vec![]], // g
+                vec![0, 0, 1, 2],                        // parent
+            )
+        );
     }
 }
