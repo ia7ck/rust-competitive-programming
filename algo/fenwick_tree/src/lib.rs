e100c0715bfbcd2fec74be1b1df307f136f58444
ia7ck <23146842+ia7ck@users.noreply.github.com>
2024-12-14 23:34:34 +0900

    e は単位元なので足しても e のまま

--- algo/fenwick_tree/src/lib.rs
+++ algo/fenwick_tree/src/lib.rs
@@ -31,15 +31,11 @@ where
     T: std::ops::SubAssign,
 {
     pub fn new(n: usize, e: T) -> Self {
-        let n = n.next_power_of_two();
-        let mut dat = vec![e; n + 1];
-        for i in (2..=n).step_by(2) {
-            let j = 1 << (i.trailing_zeros() - 1);
-            let mut y = dat[j];
-            y += dat[j];
-            dat[i] = y; // dat[j] * 2
+        Self {
+            n,
+            e,
+            dat: vec![e; n + 1],
         }
-        Self { n, e, dat }
     }
     // 0-indexed
     // a[k] += x
