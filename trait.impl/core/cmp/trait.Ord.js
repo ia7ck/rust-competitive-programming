78ddd5400bd3a66fdd2ebe69531787557d129476
ia7ck <23146842+ia7ck@users.noreply.github.com>
2026-01-25 13:41:40 +0000

    Deploying to gh-pages from @ ia7ck/rust-competitive-programming@3a596d44000358f206b50033b961902bbcfffe1a 🚀

---
+++ trait.impl/core/cmp/trait.Ord.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["arg_cmp",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"arg_cmp/enum.Quadrant.html\" title=\"enum arg_cmp::Quadrant\">Quadrant</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"arg_cmp/struct.ArgCmp.html\" title=\"struct arg_cmp::ArgCmp\">ArgCmp</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[493]}
